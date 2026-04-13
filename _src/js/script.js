5966228a6befea91b377d05420acdd93f1b20e24
saltypx <136202579+simplysalt@users.noreply.github.com>
2026-04-13 05:50:24 -0700

    aa

--- _src/js/script.js
+++ _src/js/script.js
@@ -1,28 +1,81 @@
-const cursors = {{ cursors | json }}
+import { Sortable } from "sortablejs";
+
+const cursors = {{ cursors | json }};
 
 const state = {
     assignments: {},
     unassigned: []
 };
 
+// unused. will be unecessary with my 11ty technique
+// and with reparenting drag n drop code.
 function clearHTML(...elements) {
     for (const element of elements)
     element.innerHTML = ``;
 }
 
-function render() {
+// //
+// const slots = document.getElementsByClassName(`slot`);
+// slots.forEach(slot => {
+//     slot.addEventListener('dragover', i =>
+//         i.preventDefault()); // Permit dropping
 
-    cursors.forEach(cursor => {
-        // ADD SORTABLE.JS HERE
-        // TO "DEPRECATE" EVENT LISTENERS
+//     slot.addEventListener('drop', i => {
+//         i.preventDefault();
+//         drop(i);
+//     });
+// });
 
-    slot.addEventListener('dragover', i =>
-        i.preventDefault()); // Permit dropping
+// // 
+// const slotItems = document.getElementsByClassName(`slot-item`);
+// slotItems.forEach(item => {
+//     item.addEventListener(`dragstart`, i =>
+//         i.preventDefault()); // is this even necessary?
+// });
 
-    slot.addEventListener('drop', i => {
-        i.preventDefault();
-        drop(i, cursor.id);});
+document.querySelectorAll(`.sortable-zone`).forEach(zone => {
+    new Sortable(zone, {
+        group: `cursors`,
+        animation: 0,
+        ghostClass: `sortable-ghost`,
+        onAdd: (evt) => {
+            const item = evt.item;
+            const target = evt.to.closest(`.slot`);
+
+            if (target) {
+                const cursorID = target.getAttribute(`data-cursor-id`);
+                target.setAttribute(`data-assigned`, `true`);
+                console.log(`Assigned cursor to ${cursorID}`);
+            } else {
+                evt.from.closest(`.slot`)?.setAttribute(`data-assigned`, `false`);
+            }
+        }
     });
+});
+
+window.addEventListener(`drop`, (e) => {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+
+    if (files.length > 0) {
+        files.forEach(file => {
+            if (file.name.endsWith(`.cur`) || file.name.endsWith('.ani')) {
+                addToTray(file);
+            }
+        });
+    }
+});
+
+function addToTray(file) {
+    const tray = document.getElementById(`tray-content`);
+    const div = document.createElement(`div`)
+    div.className = `cursor-item`;
+    div.innerHTML = `
+        <img src="/assets/stupid-cursor.png">
+        <div class="file-name">${file.name}</div>
+    `;
+
+    div._fileReference = file;
 }
 
 function unassign(id) {
